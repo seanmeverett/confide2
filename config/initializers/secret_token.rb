15cecfdc99a24574ef80b7ff425006fbb8f1ea00
Sean Everett <seanmeverett@gmail.com>
2013-01-16 12:13:41 -0600

    initial commit and adding Statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confide2::Application.config.secret_token = 'accdcd8f3572d120c0bc327f897efce6b994472490db9eb91f0d9e4db07ed556fcd84bc611b21d8e004cf8adcd509fd2b04f2b5848c15023ca8867e351a56916'
